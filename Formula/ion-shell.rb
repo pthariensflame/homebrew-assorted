0e111314e618c2e59c9ba96aa4a34033af988ce6
Alexander Ronald Altman <alexanderaltman@me.com>
2020-02-06 11:09:24 -0800

    ion-shell HEAD (new formula)

---
+++ Formula/ion-shell.rb
@@ -0,0 +1,33 @@
+class IonShell < Formula
+  desc "Modern system shell from RedoxOS"
+  homepage "https://gitlab.redox-os.org/redox-os/ion"
+  head "https://gitlab.redox-os.org/redox-os/ion.git"
+
+  depends_on "coreutils" => :build
+  depends_on "mdbook" => :build
+  depends_on "rust" => :build
+
+  def install
+    inreplace "Makefile", "\tinstall", "\t" + Formula["coreutils"].bin/"ginstall"
+
+    ENV["RUSTUP"] = "0"
+    ENV["TOOLCHAIN"] = Formula["rust"].version.to_s
+    ENV["prefix"] = prefix.to_s
+
+    system "make", "manual"
+    system Formula["mdbook"].bin/"mdbook", "build", "-d", doc.to_s, "manual"
+
+    system "make"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      You may need to alter /etc/shells to include #{opt_bin}/ion in order to use ion as a shell.
+    EOS
+  end
+
+  test do
+    system "ion", "--version"
+  end
+end
