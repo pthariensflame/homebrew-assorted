97ac431c2665c55d5771ff1616a24f8db3da664a
Alexander Ronald Altman <alexanderaltman@me.com>
2020-02-07 16:02:18 -0800

    Zulu Mission Control 7.1.0.65 (new cask)

---
+++ Casks/zulu-mission-control.rb
@@ -0,0 +1,12 @@
+cask 'zulu-mission-control' do
+  version '7.1.0.65'
+  sha256 '40b8208fd8b97e66f0f0c0fd93d5e8f608c4461d1d26729509e393cc7a7cbd8c'
+
+  depends_on
+  
+  url "https://cdn.azul.com/zmc/bin/zmc7.1.0.65-ca-macos_x64.tar.gz"
+  name 'Zulu Mission Control'
+  homepage 'https://www.azul.com/products/zulu-mission-control/'
+
+  app 'Zulu Mission Control.app'
+end
