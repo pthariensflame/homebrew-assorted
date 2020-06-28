c9abf9f6d326be505ce3b63bb3967eb5eb775476
Alexander Ronald Altman <alexanderaltman@me.com>
2020-06-28 14:20:50 -0700

    julia-rc 1.5.0-rc1

--- Casks/julia-rc.rb
+++ Casks/julia-rc.rb
@@ -1,6 +1,6 @@
 cask 'julia-rc' do
-  version '1.4.0-rc2'
-  sha256 '101ec0abd9474336942fe14862b9ec3018e85046e230d18d4c9b2521b93bda5a'
+  version '1.5.0-rc1'
+  sha256 'ef193bad5c458083eb84cde1a486ef34c79b00f6192e0303e51f4396f311b379'
 
   url "https://julialang-s3.julialang.org/bin/mac/x64/#{version.major_minor}/julia-#{version}-mac64.dmg"
   appcast 'https://github.com/JuliaLang/julia/releases.atom'
