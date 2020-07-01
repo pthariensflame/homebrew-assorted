6b16406cf967c654552cf62a5326448b036a6596
Alexander Ronald Altman <alexanderaltman@me.com>
2020-07-01 16:32:19 -0700

    mdbook-linkcheck 0.7.0 (new formula)

---
+++ Formula/mdbook-linkcheck.rb
@@ -0,0 +1,17 @@
+class MdbookLinkcheck < Formula
+  desc "Link-checker plugin for mdbook"
+  homepage "https://github.com/Michael-F-Bryan/mdbook-linkcheck"
+  url "https://github.com/Michael-F-Bryan/mdbook-linkcheck/archive/v0.7.0.tar.gz"
+  sha256 "e3e444690d4e2c744eb240d4f6bacc6ebfacfadd9f75c61e0c461d7f046b920e"
+
+  depends_on "rust" => :build
+  depends_on "mdbook"
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system bin/"mdbook-linkcheck", "-V"
+  end
+end
