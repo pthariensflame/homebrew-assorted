ddc31a45d72ae7095ff086258ebe567da2713c4c
Alexander Ronald Altman <alexanderaltman@me.com>
2020-07-01 16:42:51 -0700

    mdbook-linkcheck: add HEAD

--- Formula/mdbook-linkcheck.rb
+++ Formula/mdbook-linkcheck.rb
@@ -3,6 +3,7 @@ class MdbookLinkcheck < Formula
   homepage "https://github.com/Michael-F-Bryan/mdbook-linkcheck"
   url "https://github.com/Michael-F-Bryan/mdbook-linkcheck/archive/v0.7.0.tar.gz"
   sha256 "e3e444690d4e2c744eb240d4f6bacc6ebfacfadd9f75c61e0c461d7f046b920e"
+  head "https://github.com/Michael-F-Bryan/mdbook-linkcheck.git"
 
   depends_on "rust" => :build
   depends_on "mdbook"
