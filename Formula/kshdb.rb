6b2e8225bd03276e4fd44a9cc2603ce9a36357c9
Alexander Ronald Altman <alexanderaltman@me.com>
2020-07-04 21:31:40 -0700

    kshdb 1.1.0 (new formula)

---
+++ Formula/kshdb.rb
@@ -0,0 +1,25 @@
+class Kshdb < Formula
+  desc "Korn shell debugger"
+  homepage "https://kshdb.readthedocs.io/en/latest/"
+  url "https://github.com/rocky/kshdb/archive/1.1.0.tar.gz"
+  sha256 "bfc4026e80c9f55719af6d5d51349be422292da828a8190ef86260ca8ac390c3"
+  license "GPL-2.0-only"
+
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
+  depends_on "ksh"
+
+  def install
+    system "autoreconf", "-i"
+    system "autoconf"
+    system "./configure",
+           "--disable-silent-rules",
+           "--prefix=#{prefix}",
+           "--with-ksh=#{Formula["ksh"].bin/"ksh"}"
+    system "make", "install"
+  end
+
+  test do
+    system "kshdb", "-V"
+  end
+end
