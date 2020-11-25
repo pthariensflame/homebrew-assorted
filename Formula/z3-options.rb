5f6ce266c69ca5849cae1c4aa1234351dd9c94e1
Alexander Ronald Altman <alexanderaltman@me.com>
2020-11-25 15:39:36 -0800

    z3-options: fix java dependency

--- Formula/z3-options.rb
+++ Formula/z3-options.rb
@@ -4,15 +4,20 @@ class Z3Options < Formula
   url "https://github.com/Z3Prover/z3/archive/z3-4.8.9.tar.gz"
   sha256 "c9fd04b9b33be74fffaac3ec2bc2c320d1a4cc32e395203c55126b12a14ff3f4"
   license "MIT"
+  revision 1
   head "https://github.com/Z3Prover/z3.git"
   bottle :unneeded
 
-  depends_on :java => ["1.8", :optional]
+  deprecated_option "with-java" => "with-openjdk@8"
+
   depends_on "mono" => :optional
   depends_on "ocaml" => :optional
+  depends_on "openjdk" => :optional
+  depends_on "openjdk@11" => :optional
+  depends_on "openjdk@8" => :optional
   depends_on "python" => :optional
 
-  conflicts_with "z3", :because => "this is an alternate form of another formula"
+  conflicts_with "z3", because: "this is an alternate form of another formula"
 
   # This formula is for people who will compile with their chosen options
 
@@ -22,7 +27,7 @@ class Z3Options < Formula
       --prefix=#{prefix}
       --staticlib
     ]
-    
+
     if build.with? "python"
       pycmd = "python3"
       xy = Language::Python.major_minor_version "python3"
@@ -36,9 +41,17 @@ class Z3Options < Formula
       ENV["GACUTIL"] = Formula["mono"].bin/"gacutil"
       args << "--dotnet"
     end
-    args << "--java" if build.with? "java"
+
+    java_versions = [
+      (build.with? "openjdk"),
+      (build.with? "openjdk@11"),
+      (build.with? "openjdk@8"),
+    ]
+    ofail "cannot use more than one Java binding" if java_versions.count(true) > 1
+    args << "--java" if java_versions.any?
+
     args << "--ml" if build.with? "ocaml"
-    
+
     system pycmd, *args
 
     cd "build" do
