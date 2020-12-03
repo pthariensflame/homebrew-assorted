232025b2cd943ee30eecc99fcc9517f54f60e901
Alexander Ronald Altman <alexanderaltman@me.com>
2020-12-02 16:31:18 -0800

    llvmenv: 0.3.1
    And updated repo.

--- Formula/llvmenv.rb
+++ Formula/llvmenv.rb
@@ -1,9 +1,9 @@
 class Llvmenv < Formula
   desc "Manages multiple LLVM/Clang builds"
-  homepage "https://github.com/termoshtt/llvmenv"
-  url "https://github.com/termoshtt/llvmenv/archive/0.3.0.tar.gz"
-  sha256 "decf49ef354fa6567a439a0ac6e043777910aa0dda59f49c9f0aaa6f4fb9c7d5"
-  head "https://github.com/termoshtt/llvmenv.git"
+  homepage "https://github.com/llvmenv/llvmenv"
+  url "https://github.com/llvmenv/llvmenv/archive/v0.3.1.tar.gz"
+  sha256 "56ea5278db339f0f8d8cdf07fb3cf86b33a2b7343df54967d12b886bed8390d0"
+  head "https://github.com/llvmenv/llvmenv.git"
 
 
   depends_on "rust" => :build
