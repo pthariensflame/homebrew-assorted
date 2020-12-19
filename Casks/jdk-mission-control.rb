52f677313353a9e44ebb63e985456ded869c770e
Alexander Ronald Altman <alexanderaltman@me.com>
2020-12-18 23:17:37 -0800

    jdk-mission-control: dynamic URL

--- Casks/jdk-mission-control.rb
+++ Casks/jdk-mission-control.rb
@@ -2,7 +2,9 @@ cask "jdk-mission-control" do
   version "7.1.2b04"
   sha256 "ca58d200ee8a350fcc3f5c8ffe7b9b51dd8a9372de106a0ebff11f748647f986"
 
-  url "https://download.java.net/java/GA/jmc7/04/binaries/jmc-7.1.2_osx-x64.tar.gz"
+  ver_num, ver_build = version.split "b"
+
+  url "https://download.java.net/java/GA/jmc7/#{ver_build}/binaries/jmc-#{ver_num}_osx-x64.tar.gz"
   name "JDK Mission Control"
   desc "Monitor running JVMs"
   homepage "https://openjdk.java.net/projects/jmc/"
