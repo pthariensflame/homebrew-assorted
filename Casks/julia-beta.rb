f4565ab62a1fb72c61a1bd3312472461cd425e4a
Alexander Ronald Altman <alexanderaltman@me.com>
2021-01-08 16:18:55 -0800

    Update julia-beta from 1.6.0-beta1 to 1.6.0-beta1

--- Casks/julia-beta.rb
+++ Casks/julia-beta.rb
@@ -1,6 +1,6 @@
 cask "julia-beta" do
-  version "1.5.0-beta1"
-  sha256 "38636764ec87afe6367ef1baf160d6f8267a0ba87db322ab189aee228917adc1"
+  version "1.6.0-beta1"
+  sha256 "7b62a2bd17e5f9c44d0312cbbcff3a5e41d4f538ccea79d943d57e3265403014"
 
   url "https://julialang-s3.julialang.org/bin/mac/x64/#{version.major_minor}/julia-#{version}-mac64.dmg"
   appcast "https://github.com/JuliaLang/julia/releases.atom"
