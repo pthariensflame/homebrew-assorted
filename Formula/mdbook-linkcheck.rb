95eeaca01c597513c0be13ed9664ab93723f50e1
Alexander Ronald Altman <alexanderaltman@me.com>
2021-01-11 15:37:50 -0800

    mdbook-linkcheck 0.7.2

--- Formula/mdbook-linkcheck.rb
+++ Formula/mdbook-linkcheck.rb
@@ -1,8 +1,8 @@
 class MdbookLinkcheck < Formula
   desc "Link-checker plugin for mdbook"
   homepage "https://github.com/Michael-F-Bryan/mdbook-linkcheck"
-  url "https://github.com/Michael-F-Bryan/mdbook-linkcheck/archive/v0.7.1.tar.gz"
-  sha256 "349a168f7838d590163f9e03f522ae1a68f5a1eb7d6a78c24a9e8c437a05e50f"
+  url "https://github.com/Michael-F-Bryan/mdbook-linkcheck/archive/v0.7.2.tar.gz"
+  sha256 "ca9ec8cee58c1ae9ed5169da285e3254214026396351acc857f6ecb7a4186f29"
   head "https://github.com/Michael-F-Bryan/mdbook-linkcheck.git"
 
   depends_on "rust" => :build
