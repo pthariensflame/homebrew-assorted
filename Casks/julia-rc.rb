66edebd57ce04db32549a270e38e0ce387f0810c
Alexander Ronald Altman <alexanderaltman@me.com>
2021-03-17 23:52:13 -0700

    Update julia-rc from 1.6.0-rc2 to 1.6.0-rc3

--- Casks/julia-rc.rb
+++ Casks/julia-rc.rb
@@ -1,6 +1,6 @@
 cask "julia-rc" do
-  version "1.6.0-rc2"
-  sha256 "92fe7bc8d805489861e9afeb2beffa720418457f0a7dbb42004dcce1d41aa935"
+  version "1.6.0-rc3"
+  sha256 "aed1f4fe959e31b6b33d07bc7ef046c110d2522124ec0c4df7d6b423d420bf4c"
 
   url "https://julialang-s3.julialang.org/bin/mac/x64/#{version.major_minor}/julia-#{version}-mac64.dmg"
   appcast "https://github.com/JuliaLang/julia/releases.atom"
