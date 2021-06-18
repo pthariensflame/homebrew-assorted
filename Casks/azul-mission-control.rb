532e49582d83197c85544302b43dd93fddfa2463
Alexander Ronald Altman <alexanderaltman@me.com>
2021-06-18 13:03:39 -0700

    zulu-mission-control → azul-mission-control 8.0.0.17

---
+++ Casks/azul-mission-control.rb
@@ -0,0 +1,11 @@
+cask "azul-mission-control" do
+  version "8.0.0.17"
+  sha256 "111cbbd9c6fe97095985edbaef0874f26e0417a3746b608310699d6e64cac211"
+
+  url "https://cdn.azul.com/zmc/bin/zmc#{version}-ca-macos_x64.tar.gz"
+  name "Azul Mission Control"
+  desc "Azul version of JDK Mission Control; monitor running JVMs"
+  homepage "https://www.azul.com/products/components/zulu-mission-control/"
+
+  app "zmc#{version}-ca-macos_x64/Zulu Mission Control.app"
+end
