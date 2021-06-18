9481e55eaa67d0ac6257545ed25bd13f697c5758
Alexander Ronald Altman <alexanderaltman@me.com>
2021-06-18 13:09:54 -0700

    azul-mission-control: name

--- Casks/azul-mission-control.rb
+++ Casks/azul-mission-control.rb
@@ -3,7 +3,7 @@ cask "azul-mission-control" do
   sha256 "111cbbd9c6fe97095985edbaef0874f26e0417a3746b608310699d6e64cac211"
 
   url "https://cdn.azul.com/zmc/bin/zmc#{version}-ca-macos_x64.tar.gz"
-  name "Azul Mission Control"
+  name "Zulu Mission Control"
   desc "Azul version of JDK Mission Control; monitor running JVMs"
   homepage "https://www.azul.com/products/components/zulu-mission-control/"
 
