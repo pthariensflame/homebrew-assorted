db5a2e2d03d39baddfdc0b85a0fe37b1af840b86
Alexander Ronald Altman <alexanderaltman@me.com>
2021-10-21 15:48:52 -0700

    azul-mission-control:  correct app name

--- Casks/azul-mission-control.rb
+++ Casks/azul-mission-control.rb
@@ -3,9 +3,9 @@ cask "azul-mission-control" do
   sha256 "2ec100e47eb71e4b29a8a42d6d99521374bf00a83312cc965f037930f83cdee7"
 
   url "https://cdn.azul.com/zmc/bin/zmc#{version}-ca-macos_x64.tar.gz"
-  name "Zulu Mission Control"
+  name "Azul Mission Control"
   desc "Azul version of JDK Mission Control; monitor running JVMs"
   homepage "https://www.azul.com/products/components/zulu-mission-control/"
 
-  app "zmc#{version}-ca-macos_x64/Zulu Mission Control.app"
+  app "zmc#{version}-ca-macos_x64/Azul Mission Control.app"
 end
