c1a14cd71a6520cc6689db901c0e9dbb4cb16482
Alexander Ronald Altman <alexanderaltman@me.com>
2021-10-21 15:55:20 -0700

    jdk-mission-control: fix version parsing logic

--- Casks/jdk-mission-control.rb
+++ Casks/jdk-mission-control.rb
@@ -1,10 +1,8 @@
 cask "jdk-mission-control" do
-  version "7.1.2b04"
+  version "7.1.2,04"
   sha256 "ca58d200ee8a350fcc3f5c8ffe7b9b51dd8a9372de106a0ebff11f748647f986"
 
-  ver_num, ver_build = version.split "b"
-
-  url "https://download.java.net/java/GA/jmc#{version.major}/#{ver_build}/binaries/jmc-#{ver_num}_osx-x64.tar.gz"
+  url "https://download.java.net/java/GA/jmc#{version.major}/#{version.after_comma}/binaries/jmc-#{version.before_comma}_osx-x64.tar.gz"
   name "JDK Mission Control"
   desc "Monitor running JVMs"
   homepage "https://openjdk.java.net/projects/jmc/"
