1baed1ffef95c8db304b538f2c978b263523e22c
Alexander Ronald Altman <alexanderaltman@me.com>
2022-01-01 21:06:35 -0800

    alire 1.1.1 (new cask)

---
+++ Casks/alire.rb
@@ -0,0 +1,12 @@
+cask "alire" do
+  version "1.1.1"
+  sha256 "621a28b0cba4cd86efd42a7b10308515b5fbb8103e489c8876f96bcf4f715282"
+
+  url "https://github.com/alire-project/alire/releases/download/v#{version}/alr-#{version}-bin-x86_64-macos.zip",
+      verified: "github.com/alire-project"
+  name "Alire"
+  desc "Obtain, build, and incorporate Ada/SPARK libraries into your own projects"
+  homepage "https://alire.ada.dev/"
+
+  binary "bin/alr"
+end
