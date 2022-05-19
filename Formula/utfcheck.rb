1ba5630fb91ab06aabf45f86b881ac0d198b8fd2
Alexander Ronald Altman <alexanderaltman@me.com>
2022-05-19 15:45:44 -0700

    utfcheck 1.2 (new formula)

---
+++ Formula/utfcheck.rb
@@ -0,0 +1,19 @@
+class Utfcheck < Formula
+  desc "Unicode UTF-8 validation utility"
+  homepage "https://unifoundry.com/utfcheck/index.html"
+  url "https://unifoundry.com/pub/utfcheck/utfcheck-1.2/utfcheck-1.2.tar.gz"
+  sha256 "c5d5e6f6fa5d4dd1b0caa64f011d71bf5bed3f8948c2cba384e378cf3d1c0cd8"
+  license "GPL-2.0-or-later"
+
+  def install
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    system "make"
+    system "make", "check"
+    system "make", "install"
+    system "make", "clean"
+  end
+
+  test do
+    assert_predicate bin/"utfcheck", :exist?
+  end
+end
