0c2abdffa9f9a04d7a4e3565a8355002d6085b32
Alexander Ronald Altman <alexanderaltman@me.com>
2022-05-20 10:50:45 -0700

    Update alire from 1.1.2 to 1.1.3

--- Casks/alire.rb
+++ Casks/alire.rb
@@ -1,6 +1,6 @@
 cask "alire" do
-  version "1.1.2"
-  sha256 "d29ea8eeb8ac56367175f6c34e49474b6909f9f5589b978bd6f53e95b0d5224e"
+  version "1.1.3"
+  sha256 "d39a10c729cbc0f06422d64edc37dc0f45128dba93727b10a7d4d571abac3856"
 
   url "https://github.com/alire-project/alire/releases/download/v#{version}/alr-#{version}-bin-x86_64-macos.zip",
       verified: "github.com/alire-project"
