40e7bfb3bb703fc13e9e6d940695b15fd577d0fc
Alexander Ronald Altman <alexanderaltman@me.com>
2022-05-24 21:52:50 -0700

    Update azul-mission-control from 8.1.0.42 to 8.1.1.51

--- Casks/azul-mission-control.rb
+++ Casks/azul-mission-control.rb
@@ -1,6 +1,6 @@
 cask "azul-mission-control" do
-  version "8.1.0.42"
-  sha256 "2ec100e47eb71e4b29a8a42d6d99521374bf00a83312cc965f037930f83cdee7"
+  version "8.1.1.51"
+  sha256 "c147c4c0a06745b4dfb6a5e89313908eed2add5cbdd0f77a12c06b7726329573"
 
   url "https://cdn.azul.com/zmc/bin/zmc#{version}-ca-macos_x64.tar.gz"
   name "Azul Mission Control"
