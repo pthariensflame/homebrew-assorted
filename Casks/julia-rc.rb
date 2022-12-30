473fe872fc945d49fb445edaaa32afb61d124751
Laine Taffin Altman <alexanderaltman@me.com>
2022-12-29 19:37:11 -0800

    julia-rc: update livecheck

--- Casks/julia-rc.rb
+++ Casks/julia-rc.rb
@@ -3,10 +3,14 @@ cask "julia-rc" do
   sha256 "b23812d75cf01a8c5b448cfb00589e5f52d9505ec330f268aa67fb53a41ed50b"
 
   url "https://julialang-s3.julialang.org/bin/mac/x64/#{version.major_minor}/julia-#{version}-mac64.dmg"
-  appcast "https://github.com/JuliaLang/julia/releases.atom"
   name "Julia"
   desc "Programming language for technical computing"
   homepage "https://julialang.org/"
+
+  livecheck do
+    url "https://julialang.org/downloads/"
+    regex(/href=.*?julia[._-]v?(\d+(?:\.\d+)+-rc\d+)[._-]mac#{arch.delete_prefix("x")}\.dmg/i)
+  end
 
   app "Julia-#{version.major_minor}.app"
   binary "#{appdir}/Julia-#{version.major_minor}.app/Contents/Resources/julia/bin/julia", target: "julia-rc"
