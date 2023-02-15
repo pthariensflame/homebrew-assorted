e1e0ad7ff02c00e4460b20cfe8d5d5a6c572b345
Laine Taffin Altman <alexanderaltman@me.com>
2023-02-14 20:11:34 -0800

    Update julia-beta from 1.9.0-beta3 to 1.9.0-beta4

--- Casks/julia-beta.rb
+++ Casks/julia-beta.rb
@@ -1,9 +1,9 @@
 cask "julia-beta" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.9.0-beta3"
-  sha256 arm:   "5718060d136c73e53945423ef3f696e5af251ea5a46bfeba10a566f88bf65d60",
-         intel: "cae4f541a24ba8522996d5f3ee1a7978340f28bd9ad54f6fdff8227eb1b6bd7f"
+  version "1.9.0-beta4"
+  sha256 arm:   "ef01784f66110d7fffff6d138f3cf18bb0ed58cbce94572bfddb6789a664e307",
+         intel: "747b251cff81f73dc846e13f66357a0f38662f275160444b04a6383633796808"
 
   url "https://julialang-s3.julialang.org/bin/mac/#{arch}/#{version.major_minor}/julia-#{version}-mac#{arch.delete_prefix("x")}.dmg"
   name "Julia"
