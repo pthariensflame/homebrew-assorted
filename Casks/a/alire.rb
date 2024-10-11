5ff691aa12e35b61e2be584f80cac1620ce669a1
Laine Taffin Altman <alexanderaltman@me.com>
2024-10-11 13:06:00 -1000

    alire 2.0.2

--- Casks/a/alire.rb
+++ Casks/a/alire.rb
@@ -1,8 +1,12 @@
 cask "alire" do
-  version "1.2.2"
-  sha256 "7e568dfa8d9a785ef614086a1ab64d1a6853581c574ee661cffc8b558fd0bfaa"
+  arch intel: "x86_64",
+       arm:   "aarch64"
 
-  url "https://github.com/alire-project/alire/releases/download/v#{version}/alr-#{version}-bin-x86_64-macos.zip",
+  version "2.0.2"
+  sha256 intel: "d148a8efc9144b4c2f7340c272cada2c220d852ee69a07e98a5b64c4d33bd1f4",
+         arm:   "fc9eb1b2658f0dd82c73fb1980e7f2cb0e500f0846cea505b6f6231358b441ef"
+
+  url "https://github.com/alire-project/alire/releases/download/v#{version}/alr-#{version}-bin-#{arch}-macos.zip",
       verified: "github.com/alire-project/"
   name "Alire"
   desc "Obtain, build, and incorporate Ada/SPARK libraries into your own projects"
