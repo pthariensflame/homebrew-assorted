5b16cdb307196d82fa8fa74dccc6c7101f51debb
Laine Taffin Altman <alexanderaltman@me.com>
2026-02-14 12:06:55 -0800

    azul-mission-control 9.1.1.1.41

--- Casks/a/azul-mission-control.rb
+++ Casks/a/azul-mission-control.rb
@@ -2,9 +2,9 @@ cask "azul-mission-control" do
   arch arm:   "aarch64",
        intel: "x64"
 
-  version "9.1.1.35"
-  sha256 arm:   "8610486e65d187afb7d154d1950f2ce667dbc2f6d5eb6a4962368999ad382a5a",
-         intel: "9fc245aa2cb2d0fe74b76d0d957ec4527a9dbe3ce95bf760fba76089f3b14cbf"
+  version "9.1.1.1.41"
+  sha256 arm:   "3dc721d06b347f77df38368d2d1e22bdf32395cfbf47fd94226ade2802031bc5",
+         intel: "255162176eaf785bb4d922da967a528b6557392f9ca8dd5d7ebb56c5b615d7ed"
 
   url "https://cdn.azul.com/zmc/bin/zmc#{version}-ca-macos_#{arch}.tar.gz"
   name "Azul Mission Control"
