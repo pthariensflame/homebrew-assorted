2b3492466ae13354328c942280b405930aad2cc8
Laine Taffin Altman <alexanderaltman@me.com>
2026-09-09 15:45:46 -0700

    stoat 1.5.3

--- Casks/s/stoat.rb
+++ Casks/s/stoat.rb
@@ -2,9 +2,9 @@ cask "stoat" do
   arch arm:   "arm64",
        intel: "x64"
 
-  version "1.4.2"
-  sha256 arm:   "5d84f90a970a0e5304f6c0a0f06a892590d8f2d35c8e57eca97e342ebc11b1f7",
-         intel: "62cbe307229b7c20c8d2b5534ed2972bc788ef44a70855a4311c31075841e375"
+  version "1.5.3"
+  sha256 arm:   "ec031ea68abae09acebcad9135879ec33917be01fcb10bbb691f458b9103236d",
+         intel: "342922602d21243dfb24a28551d696d5101ee4fb0426d2540d720df8158031a3"
 
   url "https://github.com/stoatchat/for-desktop/releases/download/v#{version}/Stoat-darwin-#{arch}-#{version}.zip"
   name "Stoat"
