5a284ff59151232c30777be1b970f1a70f5dff2f
Laine Taffin Altman <alexanderaltman@me.com>
2026-09-09 15:42:40 -0700

    openvadl 0.3.0

--- Formula/o/openvadl.rb
+++ Formula/o/openvadl.rb
@@ -1,8 +1,8 @@
 class Openvadl < Formula
   desc "Open-source implementation of the VADL processor description language"
   homepage "https://openvadl.org"
-  url "https://github.com/OpenVADL/openvadl/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "1a8228b870a58c53fa7b650d7d169de3909971cabeedb9018cbc10f56f8b7e0b"
+  url "https://github.com/OpenVADL/openvadl/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "75279ec3d89472796200bff8eb108649afe98d7cf7a88d39cb24bf5961a88499"
   license all_of: ["GPL-3.0-or-later", "Apache-2.0", "CC-BY-4.0"]
   head "https://github.com/OpenVADL/openvadl.git", branch: "master"
 
